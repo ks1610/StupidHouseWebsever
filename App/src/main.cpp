2974566915d166ad4885d2b4f874f30ca8afc0ae
Khanh (Spite) <118985514+ks1610@users.noreply.github.com>
2024-05-11 05:41:18 +0700

    Update main.cpp

--- App/src/main.cpp
+++ App/src/main.cpp
@@ -20,7 +20,7 @@ String output27State = "off";
 String output25State = "off";
 String output33State = "off";
 String output32State = "off";
-String output34State = "off";
+String output14State = "off";
 
 // Assign output variables to GPIO pins
 const int output26 = 26;
@@ -28,7 +28,7 @@ const int output27 = 27;
 const int output25 = 25;
 const int output33 = 33;
 const int output32 = 32;
-const int output34 = 34;
+const int output14 = 14;
 
 // Current time
 unsigned long currentTime = millis();
@@ -52,14 +52,14 @@ void setup() {
   pinMode(output25, OUTPUT);
   pinMode(output33, OUTPUT);
   pinMode(output32, OUTPUT);
-  pinMode(output34, OUTPUT);
+  pinMode(output14, OUTPUT);
   // Set outputs to LOW
   digitalWrite(output26, LOW);
   digitalWrite(output27, LOW);
   digitalWrite(output25, LOW);
   digitalWrite(output33, LOW);
   digitalWrite(output32, LOW);
-  digitalWrite(output34, LOW);
+  digitalWrite(output14, LOW);
 
   // Connect to Wi-Fi network with SSID and password
   Serial.print("Connecting to ");
@@ -143,14 +143,14 @@ void loop(){
                 Serial.println("GPIO 32 off");
                 output32State = "off";
                 digitalWrite(output32, LOW);
-            } else if (header.indexOf("GET /34/on") >= 0) {
-                Serial.println("GPIO 34 on");
-                output34State = "on";
-                digitalWrite(output34, HIGH);
-            } else if (header.indexOf("GET /34/off") >= 0) {
-                Serial.println("GPIO 34 off");
-                output34State = "off";
-                digitalWrite(output34, LOW);
+            } else if (header.indexOf("GET /14/on") >= 0) {
+                Serial.println("GPIO 14 on");
+                output14State = "on";
+                digitalWrite(output14, HIGH);
+            } else if (header.indexOf("GET /14/off") >= 0) {
+                Serial.println("GPIO 14 off");
+                output14State = "off";
+                digitalWrite(output14, LOW);
             }
 
             
@@ -210,12 +210,12 @@ void loop(){
                 client.println("<p><a href=\"/32/off\"><button class=\"button button2\">OFF</button></a></p>");
             }
 
-            // Display current state, and ON/OFF buttons for GPIO 34
-            client.println("<p>GPIO 34 - State " + output34State + "</p>");
-            if (output34State == "off") {
-                client.println("<p><a href=\"/34/on\"><button class=\"button\">ON</button></a></p>");
+            // Display current state, and ON/OFF buttons for GPIO 14
+            client.println("<p>GPIO 14 - State " + output14State + "</p>");
+            if (output14State == "off") {
+                client.println("<p><a href=\"/14/on\"><button class=\"button\">ON</button></a></p>");
             } else {
-                client.println("<p><a href=\"/34/off\"><button class=\"button button2\">OFF</button></a></p>");
+                client.println("<p><a href=\"/14/off\"><button class=\"button button2\">OFF</button></a></p>");
             }
             client.println("</body></html>");
             
@@ -262,7 +262,7 @@ String output27State = "off";
 String output25State = "off";
 String output33State = "off";
 String output32State = "off";
-String output34State = "off";
+String output14State = "off";
 
 // Assign output variables to GPIO pins
 const int output26 = 26;
@@ -270,7 +270,7 @@ const int output27 = 27;
 const int output25 = 25;
 const int output33 = 33;
 const int output32 = 32;
-const int output34 = 34;
+const int output14 = 14;
 
 // Current time
 unsigned long currentTime = millis();
@@ -294,14 +294,14 @@ void setup() {
   pinMode(output25, OUTPUT);
   pinMode(output33, OUTPUT);
   pinMode(output32, OUTPUT);
-  pinMode(output34, OUTPUT);
+  pinMode(output14, OUTPUT);
   // Set outputs to LOW
   digitalWrite(output26, LOW);
   digitalWrite(output27, LOW);
   digitalWrite(output25, LOW);
   digitalWrite(output33, LOW);
   digitalWrite(output32, LOW);
-  digitalWrite(output34, LOW);
+  digitalWrite(output14, LOW);
 
   // Connect to Wi-Fi network with SSID and password
   Serial.print("Connecting to ");
@@ -385,14 +385,14 @@ void loop(){
                 Serial.println("GPIO 32 off");
                 output32State = "off";
                 digitalWrite(output32, LOW);
-            } else if (header.indexOf("GET /34/on") >= 0) {
-                Serial.println("GPIO 34 on");
-                output34State = "on";
-                digitalWrite(output34, HIGH);
-            } else if (header.indexOf("GET /34/off") >= 0) {
-                Serial.println("GPIO 34 off");
-                output34State = "off";
-                digitalWrite(output34, LOW);
+            } else if (header.indexOf("GET /14/on") >= 0) {
+                Serial.println("GPIO 14 on");
+                output14State = "on";
+                digitalWrite(output14, HIGH);
+            } else if (header.indexOf("GET /14/off") >= 0) {
+                Serial.println("GPIO 14 off");
+                output14State = "off";
+                digitalWrite(output14, LOW);
             }
 
             
@@ -407,7 +407,7 @@ void loop(){
             client.println(".button_26{background-image: url(\"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSNiv_i0z7SUFMXKZYbYMlB_kNrSsj1VvfywA6Y0Y-row&s\"); background-size: cover;background-position: center top;}");
             client.println(".button_27{background-image: url(\"https://t3.ftcdn.net/jpg/02/08/25/66/360_F_208256678_37zjgDuOcgPgmV7mpRVJr1hKQp6te9dY.webp\");background-size: cover;background-position: center top;}");
             client.println(".button_25, .button_33, .button_32{background-image: url(\"https://www.shutterstock.com/image-vector/double-bed-lamp-showing-bedroom-260nw-1190868772.jpg\");background-size: cover;background-position: center top;}");
-            client.println(".button_34{background-image: url(\"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRpWN_GZLJ6HYcGa8XOeMXp_6T8i-0iekMcf-NwEtx6kQ&s\");background-size: cover;background-position: center top;}");
+            client.println(".button_14{background-image: url(\"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRpWN_GZLJ6HYcGa8XOeMXp_6T8i-0iekMcf-NwEtx6kQ&s\");background-size: cover;background-position: center top;}");
             client.println(".gpio-container-1, .gpio-container-2, .gpio-container-3 {display: flex; margin: 20px;flex-direction: row; justify-content: center;}");
             client.println(".gpio{ flex: 1; margin: 0 10px;}");
             client.println(".button2 {background-color: #555555;}</style></head>");
@@ -480,12 +480,12 @@ void loop(){
                 client.println("<p><a href=\"/32/off\"><button class=\"button button2\">OFF</button></a></p>");
             }
 
-            // Display current state, and ON/OFF buttons for GPIO 34
-            client.println("<p>GPIO 34 - State " + output34State + "</p>");
-            if (output34State == "off") {
-                client.println("<p><a href=\"/34/on\"><button class=\"button\">ON</button></a></p>");
+            // Display current state, and ON/OFF buttons for GPIO 14
+            client.println("<p>GPIO 14 - State " + output14State + "</p>");
+            if (output14State == "off") {
+                client.println("<p><a href=\"/14/on\"><button class=\"button\">ON</button></a></p>");
             } else {
-                client.println("<p><a href=\"/34/off\"><button class=\"button button2\">OFF</button></a></p>");
+                client.println("<p><a href=\"/14/off\"><button class=\"button button2\">OFF</button></a></p>");
             }
             */
            /*
@@ -498,13 +498,13 @@ void loop(){
                 client.println("<p><a href=\"/32/off\"><button class=\"button button2\">Bed Room</button></a></p></div>");
             } 
 
-            // Display current state, and ON/OFF buttons for GPIO 34  
-            client.println("<div><p>Garden - State " + output34State + "</p>");
-            // If the output34State is off, it displays the ON button       
-            if (output34State=="off") {
-                client.println("<p><a href=\"/34/on\"><button class=\"button\">Garden</button></a></p>");
+            // Display current state, and ON/OFF buttons for GPIO 14  
+            client.println("<div><p>Garden - State " + output14State + "</p>");
+            // If the output14State is off, it displays the ON button       
+            if (output14State=="off") {
+                client.println("<p><a href=\"/14/on\"><button class=\"button\">Garden</button></a></p>");
             } else {
-                client.println("<p><a href=\"/34/off\"><button class=\"button button2\">Garden</button></a></p></div></div>");
+                client.println("<p><a href=\"/14/off\"><button class=\"button button2\">Garden</button></a></p></div></div>");
             }*/
 
             // Display current state, and ON/OFF buttons for GPIO 26 and 27 in the same line
@@ -543,7 +543,7 @@ void loop(){
             }
             client.println("</div></div>");
 
-            // Display current state, and ON/OFF buttons for GPIO 32 and 34 in the same line
+            // Display current state, and ON/OFF buttons for GPIO 32 and 14 in the same line
             client.println("<div class=\"gpio-container-3\"><div class=\"gpio\"><p>Bedroom 3 - State " + output32State + "</p>");
             // If the output32State is off, it displays the ON button       
             if (output32State=="off") {
@@ -551,13 +551,13 @@ void loop(){
             } else {
               client.println("<p><a href=\"/32/off\"><button class=\"button_32 button2\"></button></a></p>");
             } 
-            // Display current state, and ON/OFF buttons for GPIO 34
-            client.println("</div><div class=\"gpio\"><p>Garden - State " + output34State + "</p>");
-            // If the output34State is off, it displays the ON button       
-            if (output34State=="off") {
-              client.println("<p><a href=\"/34/on\"><button class=\"button_34\"></button></a></p>");
+            // Display current state, and ON/OFF buttons for GPIO 14
+            client.println("</div><div class=\"gpio\"><p>Garden - State " + output14State + "</p>");
+            // If the output14State is off, it displays the ON button       
+            if (output14State=="off") {
+              client.println("<p><a href=\"/14/on\"><button class=\"button_14\"></button></a></p>");
             } else {
-              client.println("<p><a href=\"/34/off\"><button class=\"button_34 button2\"></button></a></p>");
+              client.println("<p><a href=\"/14/off\"><button class=\"button_14 button2\"></button></a></p>");
             }
             client.println("</div></div>");
 
